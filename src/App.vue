861f57104358d6c96e7963bbb091f5681bf82db3
90rimane <rima.aneta@gmail.com>
2023-02-06 11:22:15 +0100

    first initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import CitiesList from './components/CitiesList.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import TenCurrencies from './components/TenCurrencies.vue';
+  import TopMenu from './components/TopMenu.vue';
+
+  export default {
+    components: {
+      CitiesList,
+      HelloWorld,
+      TenCurrencies,
+      TopMenu
+    }
+  }
+</script>
+
+<template>
+  <HelloWorld msg="Hello World!" />
+  <CitiesList />
+  
+  <TopMenu />
+  <TenCurrencies wellMsg="Välkommen till Valuta sidan" />
+</template>
---
+++ src/components/TenCurrencies.vue
@@ -0,0 +1,65 @@
+<script>
+  export default {
+    created() {
+        fetch('https://api.freecurrencyapi.com/v1/currencies?'+
+            'apikey=9rxF6i8YuMa16mWAH3wU7SKFtaAQ3akw8zT1l6Ff&'+
+            'currencies=THB%2CNOK%2CTRY%2CHKD%2CBGN%2CEUR%2CCNY%2CCAD%2CSEK%2CBRL%2CCAD%2CUSD')
+        .then((response) => response.json())
+        .then((result) =>{
+        this.currencies = result.data;
+        keyes = Object.keys(this.currencies)
+        console.log(keyes)
+        })
+    },
+    data(){
+      return {
+          backgroundImage: 'background.jpg',
+          currencies: null
+      }
+    },
+    props: {
+      wellMsg: String
+    }
+  }
+</script>
+
+<style>
+  table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    background-color: aliceblue;
+  }
+  th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #e9e9e9;
+  }
+  tr:hover {
+    background-color: coral;
+  }
+  
+</style>
+
+<template>
+  <dl v-if="currencies !== null">
+            <h2>{{ wellMsg }} </h2>
+            <table>
+                <tr>
+                    <th>Valutakod</th>
+                    <th>Namn</th>
+                    <th>Symbol</th> 
+                    <th>Plural namn</th>
+                </tr>
+                    <template v-for="(currency, key ) in currencies" >
+                <tr>
+                    <td> {{key}} </td> 
+                    <td> {{ currency.name}} </td>
+                    <td> {{ currency.symbol}} </td>
+                    <td> {{ currency.name_plural}} </td>
+                </tr>    
+                    </template>
+            </table>
+        </dl>
+        <p v-else>Laddar...</p>
+</template>
